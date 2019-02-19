066ee5f92e32dc832d1947c6bdfe312b6dad9fda
Ameer Abdulaziz <ameer@idealisticsolutins.com>
2019-02-19 21:13:34 +0200

    Updates

---
+++ precache-manifest.4d1d8c8800932a62550767f85c3c8e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85e3bf223889503391a1",
+    "url": "/contact-manager/static/js/runtime~main.85e3bf22.js"
+  },
+  {
+    "revision": "00237a87dc0b42bf1b91",
+    "url": "/contact-manager/static/js/main.00237a87.chunk.js"
+  },
+  {
+    "revision": "ace483a88f558d31b02c",
+    "url": "/contact-manager/static/js/2.ace483a8.chunk.js"
+  },
+  {
+    "revision": "00237a87dc0b42bf1b91",
+    "url": "/contact-manager/static/css/main.954a43d8.chunk.css"
+  },
+  {
+    "revision": "ace483a88f558d31b02c",
+    "url": "/contact-manager/static/css/2.8eac295d.chunk.css"
+  },
+  {
+    "revision": "473e2d5296ad276709f13111517884f0",
+    "url": "/contact-manager/index.html"
+  }
+];
